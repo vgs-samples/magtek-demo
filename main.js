319efa4997ae1d054a08f0ef17eaa5b5b1f8d066
Alexandr <Alexandr.Florinskij@yandex.ua>
2019-04-23 17:28:13 +0300

    new user flow

--- main.js
+++ main.js
@@ -2,48 +2,53 @@ var devideID;
 
 $(document).ready(function() {
 	$.ajax({
-      method: 'POST',
-      url: 'http://localhost:9002/api/mtscrahost/RequestDeviceList',
-      data: {
-        'WaitTime': 10,
-        'ConnectionType': 4,
-      },
-      success: function (result) {
-        console.log('success', result)
-        devideID = result.DeviceList[0].Value;
-      },
-      error: function(result) {
-        console.log('error', result)
-      }
-    });
+    method: 'POST',
+    url: 'http://localhost:9002/api/mtscrahost/RequestDeviceList',
+    data: {
+      'WaitTime': 10,
+      'ConnectionType': 4,
+    },
+    success: function (result) {
+      console.log('success', result)
+      devideID = result.DeviceList[0].Value
+      $('#devide_id').html("<b>Device ID: </b>" + devideID).removeClass('blink');
+      $('#dipResultWr').show();
+    },
+    error: function(result) {
+      console.log('error', result)
+    }
+  });
 })
 
 
-function doSwipe() {
+function doDIP() {
   return new Promise( (resolve, reject) => {
-    $('#swipeBtn').get(0).disabled=true;
+    $("#swipeResultWr").hide();
+    $('#dipRequestWr').show();
+    $('#dip_request').hide();
     $.ajax({
       method: 'POST',
-      url: 'http://localhost:9002/api/mtscrahost/RequestCardSwipe',
+      url: 'http://localhost:9002/api/mtscrahost/RequestSmartCard',
       data: {
-        'WaitTime': 10,
+        'WaitTime': 20,
         'ConnectionType': 4,
         'DeviceID': devideID,
+        'TransactionType': '0x00',
+        'CardType': 3,
+        'Amount': 10,
+        'CashBack': 0,
+        'CurrencyCode': '0840',
+        'ReportOptions': 2,
+        'Options': 0
       },
       success: function (result) {
-        console.log('success', result)
-        
-        $('#swipeBtn').get(0).disabled=false;
-        $('#swipeBtnWr').hide();
-        $('#swipeResultWr').show();
-                
-        $('#swipeResult').val(JSON.stringify(result.CardSwipeOutput, undefined, 4));
-        $('#last4').html(result.CardSwipeOutput.CardLast4);
-        $('#ccName').html(result.CardSwipeOutput.CardName);
-        resolve();
+        console.log('success DIP', result)
+        $('#dipResponceWr').show();
+        $('#dipRequestWr').hide();
+        resolve(result);
       },
-      error: function(result) {
-        console.log('error', result)
+      error: function(error) {
+        console.log('error', error)
         $('#swipeBtn').get(0).disabled=false;
         $('#dipRequestWr').hide();
         $("#error").show();
@@ -53,62 +58,45 @@ function doSwipe() {
 }
 
 function requestDIP() {
-  return new Promise( (resolve, reject) => {
-    $("#swipeResultWr").hide();
-    $('#dipRequestWr').show();
-    $.ajax({
-      method: 'POST',
-      url: 'http://localhost:9002/api/mtscrahost/RequestSmartCard',
-      data: {
-          'WaitTime': 20,
-          'ConnectionType': 4,
-          'DeviceID': devideID,
-          'TransactionType': '0x00',
-          'CardType': 3,
-          'Amount': 10,
-          'CashBack': 0,
-          'CurrencyCode': '0840',
-          'ReportOptions': 2,
-          'Options': 0
-      },
-      success: function (result) {
-        console.log('success', result)
-        $('#dipResultWr').show();
-        $('#dipRequestWr').hide();
-        collectInit(JSON.stringify(result));
-        resolve();
-      },
-      error: function(result) {
-        console.log('error', result)
-        $('#swipeBtn').get(0).disabled=false;
-        $('#dipRequestWr').hide();
-        $("#error").show();
-      }
-    });
+  doDIP().then(result => {
+    console.log('DIP DATA', result)
+    $('#DIP_Data_result').show().html(result.ResponseOutput.ARQCData);
+    collectInit(result.ResponseOutput.ARQCData)
   });
 }
 
-function requestSwipe() {
-  doSwipe();
-}
+function collectInit (arqc) {
+  var f = VgForm.create('tntn962vs90', function (state) {});
 
-function collectInit (cardData) {
-  var f = VgForm.create('tntjelgjqav', function (state) {});
-
-  const secret = f.field('#cardData .fake-input', {
-    type: 'textarea',
-    name: 'secret',
-    defaultValue: cardData,
-    validations: ['required']
+  f.field('#cardData .fake-input', {
+    type: 'card-security-code',
+    name: 'cvv',
+    placeholder: 'cvv',
+    validations: ['required', 'validCardSecurityCode'],
   });
   
   document.getElementById('ccForm').addEventListener('submit', function(e) {
     e.preventDefault();
-    f.submit('/post', {
+    $('#submit_button').addClass('is-loading')
+    f.submit('/decrypt', {
+      data: {
+        arqc: arqc,
+      }
     }, function(status, data) {
-      $('#collectResultWr').show();
-      $('#dipResultWr').hide();
-      $('#collectResult').html(JSON.stringify(data.json.secret, null, '  '));
+      console.log('data', data)
+      $('#submit_button').removeClass('is-loading')
+      $('#dipResponceWr').hide()
+      $('#CollectResponceWr').show()
+      $('#CollectResponce').html(`
+      
+      curl https://echo.apps.verygood.systems/post -k \\
+      -x US4uMsMXjxHRByT2sQWHzJW8:7354c963-484f-45af-9aa0-6430e1968834@tntn962vs90.sandbox.verygoodproxy.com:8080 \\
+      -H "Content-type: application/json" \\
+      -d '${JSON.stringify(data)}'
+      `);
+      $('pre code').each(function(i, block) {
+        hljs.highlightBlock(block);
+      });
     });
   }, false);
 };
